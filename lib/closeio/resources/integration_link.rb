0ee5f7f5af330c8cfb48c36d9c9bcbcb666597df
Taylor Brooks <tbrooks@gmail.com>
2016-04-27 13:02:40 -0500

    Fix path

--- lib/closeio/resources/integration_link.rb
+++ lib/closeio/resources/integration_link.rb
@@ -3,28 +3,28 @@ module Closeio
     module IntegrationLink
 
       def list_integration_links
-        get(custom_field_path)
+        get(integration_link_path)
       end
 
       def find_integration_link(id)
-        get("#{custom_field_path}#{id}/")
+        get("#{integration_link_path}#{id}/")
       end
 
       def create_integration_link(options={})
-        post(custom_field_path, options)
+        post(integration_link_path, options)
       end
 
       def update_integration_link(id, options={})
-        put("#{custom_field_path}#{id}/", options)
+        put("#{integration_link_path}#{id}/", options)
       end
 
       def delete_integration_link(id)
-        delete("#{custom_field_path}#{id}/")
+        delete("#{integration_link_path}#{id}/")
       end
 
       private
 
-      def custom_field_path
+      def integration_link_path
         "integration_link/"
       end
 
